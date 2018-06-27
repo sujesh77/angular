6b51e13f569bfa96ecf1e9b08627b762879093f3
sujesh <sujesh.acc@hotmail.com>
2018-06-27 13:28:05 +0545

    Fetch mock data by synchronous signature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 
 @Component({
@@ -12,21 +12,23 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Sujesh'
-  //
-  // };
 
-  constructor() {
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
+  constructor(private heroService: HeroService) {
+  }
+
+
   ngOnInit() {
+    this.getHeroes();
+
   }
 
 }

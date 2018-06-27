9a9af397ee8d23fb86b2b8aff9a062ed3618330d
sujesh <sujesh.acc@hotmail.com>
2018-06-27 15:29:00 +0545

    Service class completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,8 +18,12 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   constructor(private heroService: HeroService) {
